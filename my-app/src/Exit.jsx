71dd1f32d06144e46ca3f42fafe853e624466412
lacyelln <lacyellenmiller@gmail.com>
2025-10-15 11:45:52 -0600

    added an exit closing page

---
+++ my-app/src/Exit.jsx
@@ -0,0 +1,9 @@
+export function Exit(){
+    return (
+        <div className="survey-container">
+            <p>Thank you for completing the survey, you may now exit.</p>
+        </div>
+    )
+}
+
+export default Exit;
